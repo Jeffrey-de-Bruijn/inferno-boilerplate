88dd5509640aa272f26bf0643a807b3d018f6338
Jeffrey de Bruijn <jeffrey@mamanissaffron.com>
2022-08-21 10:33:45 +0200

    Solve issue #2

--- webpack.config.js
+++ webpack.config.js
@@ -7,45 +7,62 @@ const isProduction = process.env.NODE_ENV == 'production';
 const stylesHandler = 'style-loader';
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new WindiCSSWebpackPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-                test: /\.less$/i,
-                use: ['less-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		open: true,
+		host: 'localhost',
+	},
+	plugins: [
+		new WindiCSSWebpackPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.less$/i,
+				use: ['less-loader'],
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: 'asset',
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							"@babel/preset-flow"
+						],
+						plugins: [
+							[
+								"babel-plugin-inferno",
+								{
+									"imports": true
+								}
+							]
+						]
+					}
+				}
+			}
+		],
+	},
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+	if (isProduction) {
+		config.mode = 'production';
+
+
+	} else {
+		config.mode = 'development';
+	}
+	return config;
 };
