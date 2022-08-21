cadf02eb37ba04270ec85cbd4f26bb0ee4a703e3
Jeffrey de Bruijn <jeffrey@mamanissaffron.com>
2022-08-21 10:49:17 +0200

    fix webpack output filename

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ const config = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
 	},
 	devServer: {
 		open: true,
@@ -22,7 +23,7 @@ const config = {
 		rules: [
 			{
 				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
+				use: [stylesHandler, 'css-loader'],
 			},
 			{
 				test: /\.less$/i,
