7569dab0bd870121611b5c9f254771c955d72482
Jeffrey de Bruijn <jeffrey@mamanissaffron.com>
2022-08-24 11:49:34 +0200

    Do not watch files in node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -7,80 +7,84 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const isProduction = process.env.NODE_ENV == 'production';
 
 const config = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	devServer: {
-		hot: true,
-		host: 'localhost',
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: './src/index.html'
-		}),
-		new WindiCSSWebpackPlugin(),
-		new MiniCssExtractPlugin(),
-	],
-	optimization: {
-		minimizer: [
-			`...`,
-			new CssMinimizerPlugin(),
-		],
-	},
-	module: {
-		rules: [{
-			// https://webpack.js.org/guides/asset-modules/
-			test: /\.(png|jpe?g|gif|svg|ttf|woff2)$/i,
-			type: "asset/resource",
-		},
-		{
-			test: /\.m?jsx?$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: [
-						"@babel/preset-env",
-						"@babel/preset-flow"
-					],
-					plugins: [
-						[
-							"babel-plugin-inferno",
-							{
-								"imports": true
-							}
-						]
-					]
-				}
-			}
-		},
-		{
-			test: /\.(le|c)ss$/i,
-			use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
-		},
-		{
-			test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-			type: 'asset',
-		}],
-	},
-	resolve: {
-		alias: {
-			'react': 'inferno-compat',
-			'react-dom': 'inferno-compat'
-		}
-	}
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    hot: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new WindiCSSWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+  module: {
+    rules: [{
+      // https://webpack.js.org/guides/asset-modules/
+      test: /\.(png|jpe?g|gif|svg|ttf|woff2)$/i,
+      type: "asset/resource",
+    },
+    {
+      test: /\.m?jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-flow"
+          ],
+          plugins: [
+            [
+              "babel-plugin-inferno",
+              {
+                "imports": true
+              }
+            ]
+          ]
+        }
+      }
+    },
+    {
+      test: /\.(le|c)ss$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+    },
+    {
+      test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+      type: 'asset',
+    }],
+  },
+  // Prevent watching files in the node_modules directory
+  watchOptions: {
+    ignored: '**/node_modules',
+  },
+  resolve: {
+    alias: {
+      'react': 'inferno-compat',
+      'react-dom': 'inferno-compat'
+    }
+  }
 };
 
 module.exports = () => {
-	if (isProduction) {
-		config.mode = 'production';
+  if (isProduction) {
+    config.mode = 'production';
 
 
-	} else {
-		config.mode = 'development';
-	}
-	return config;
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
